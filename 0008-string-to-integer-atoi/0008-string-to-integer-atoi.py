815b8802a5f4ce57d3eb07c5fb46bf257f59064a
Sahil Bhayre <123098sahilbhayre@gmail.com>
2026-02-13 14:04:31 +0530

    Time: 6 ms (22.25%), Space: 12.5 MB (57.96%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+        
+        i = 0
+        n = len(s)
+        
+        # Skip leading whitespaces
+        while i < n and s[i] == ' ':
+            i += 1
+        
+        # If string is empty after removing spaces
+        if i == n:
+            return 0
+        
+        # Handle sign
+        sign = 1
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+        
+        # Convert digits
+        result = 0
+        
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+            
+            # Check overflow before multiplying
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+            
+            result = result * 10 + digit
+            i += 1
+        
+        return sign * result    
