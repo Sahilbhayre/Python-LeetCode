5eca0bac357fb135a864fe9d46aa035e0600684b
Sahil Bhayre <123098sahilbhayre@gmail.com>
2026-02-13 14:31:52 +0530

    Time: 16 ms (43.11%), Space: 12.6 MB (35.39%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -10,7 +10,6 @@ class Solution(object):
         dp = [[False] * (n + 1) for _ in range(m + 1)]
         dp[0][0] = True
         
-        # Handle patterns like a*, a*b*, a*b*c*
         for j in range(2, n + 1):
             if p[j - 1] == '*':
                 dp[0][j] = dp[0][j - 2]
@@ -22,10 +21,8 @@ class Solution(object):
                     dp[i][j] = dp[i - 1][j - 1]
                 
                 elif p[j - 1] == '*':
-                    # zero occurrence
                     dp[i][j] = dp[i][j - 2]
                     
-                    # one or more occurrence
                     if p[j - 2] == s[i - 1] or p[j - 2] == '.':
                         dp[i][j] |= dp[i - 1][j]
         
