682e3a0fd14e1039cebdcb7cb00bca8b0ee8e68b
Sahil Bhayre <123098sahilbhayre@gmail.com>
2026-02-18 18:16:18 +0530

    Time: 367 ms (5.25%), Space: 67.7 MB (8.08%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.py
+++ 1148-article-views-i/1148-article-views-i.py
@@ -4,9 +4,10 @@ def article_views(views: pd.DataFrame) -> pd.DataFrame:
 #     result = views[views['author_id'] == views['viewer_id']][['author_id']].drop_duplicates().sort_values('author_id').rename(columns={'author_id':'id'})
 #     return result
 
-    mask = views["author_id"] == views["viewer_id"]
-    result = views.loc[mask, ["author_id"]] \
-                  .drop_duplicates() \
-                  .sort_values("author_id") \
-                  .rename(columns={"author_id": "id"})
+    result = (
+        views.query("author_id == viewer_id")[["author_id"]]
+        .drop_duplicates()
+        .sort_values("author_id")
+        .rename(columns={"author_id": "id"})
+    )
     return result
